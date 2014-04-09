a0f48dfba5f7a7f30999518801e580e16489d594
Todd Rinaldo <toddr@cpan.org>
2014-04-09 17:33:22 -0500

    Correct POD error from upstream fixes
    blib/lib/Exporter.pm (165): Unterminated C< ... > sequence

--- lib/Exporter.pm
+++ lib/Exporter.pm
@@ -163,7 +163,7 @@ try to use C<@EXPORT_OK> in preference to C<@EXPORT> and avoid short or
 common symbol names to reduce the risk of name clashes.
 
 Generally anything not exported is still accessible from outside the
-module using the C<YourModule::item_name> (or C<< $blessed_ref->method>>)
+module using the C<YourModule::item_name> (or C<< $blessed_ref->method >>)
 syntax.  By convention you can use a leading underscore on names to
 informally indicate that they are 'internal' and not for public use.
 
